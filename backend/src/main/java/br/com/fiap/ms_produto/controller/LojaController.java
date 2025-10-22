14b176f178a705820b3ec982fde13e028e6ef94e
CarvalhoGL <72374403+CarvalhoGL@users.noreply.github.com>
2025-10-22 11:56:11 -0300

    Add files via upload

---
+++ backend/src/main/java/br/com/fiap/ms_produto/controller/LojaController.java
@@ -0,0 +1,25 @@
+package br.com.fiap.ms_produto.controller;
+
+import br.com.fiap.ms_produto.dto.LojaDTO;
+import br.com.fiap.ms_produto.service.LojaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/lojas")
+public class LojaController {
+
+    @Autowired
+    private LojaService service;
+
+    @GetMapping
+    public ResponseEntity<List<LojaDTO>> findAçç(){
+        List<LojaDTO> dto = service.findAll();
+        return  ResponseEntity.ok(dto);
+    }
+}
